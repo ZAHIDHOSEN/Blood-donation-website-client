9cf33b8689f383f1f59b5f2c967274e3cd53263e
ZAHIDHOSEN <zahidhosen203@gmail.com>
2025-01-14 21:24:35 +0600

    added basic register from

--- src/Route/Router.jsx
+++ src/Route/Router.jsx
@@ -6,6 +6,7 @@ import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
 import ErrorPage from "../Pages/Error/ErrorPage";
 import Search from "../Pages/Search/Search";
+import Register from "../Pages/Register/Register";
 
 
  export const router = createBrowserRouter([
@@ -22,6 +23,21 @@ import Search from "../Pages/Search/Search";
           path: 'search',
           element: <Search></Search>,
           loader: () => fetch('http://localhost:5000/district')
+        },
+        {
+          path: 'register',
+          element: <Register></Register>,
+          loader: async () =>{
+            const [district, upazila] = await Promise.all([
+              fetch('http://localhost:5000/district')
+              .then(res => res.json()),
+              fetch('http://localhost:5000/upazila')
+              .then(res => res.json())
+            ]);
+            return {district, upazila};
+          }
+
+          
         }
       ]
    
